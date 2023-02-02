bb064fdce15c6d2c1e82a550844609814f35d4b9
iijung <987kmj4@naver.com>
2023-02-03 06:46:04 +0900

    feat: add prompt #2

--- src/minishell.c
+++ src/minishell.c
@@ -6,23 +6,55 @@
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 23:20:36 by minjungk          #+#    #+#             */
-/*   Updated: 2023/01/24 23:35:35 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/02/03 06:36:15 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static volatile sig_atomic_t	g_status;
+
+void	handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+		g_status = 130;
+	}
+	else if (sig == SIGQUIT)
+	{
+		write(STDOUT_FILENO, "\b\b  \b\b", 6);
+		g_status = 0;
+	}
+}
+
 int	main(void)
 {
 	char	*line;
+	char	*command;
 
+	signal(SIGINT, handler);
+	signal(SIGQUIT, handler);
 	while (1)
 	{
 		line = readline("minishell$ ");
 		if (line == NULL)
-			return (0);
-		add_history(line);
+		{
+			ft_putstr_fd("exit\n", STDOUT_FILENO);
+			break ;
+		}
+		command = ft_strtrim(line, " ");
+		if (command && *command)
+		{
+			add_history(command);
+			printf("[TODO] execute[%s]\n", command);
+		}
+		free(command);
 		free(line);
 	}
-	return (0);
+	rl_clear_history();
+	exit(EXIT_SUCCESS);
 }
--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 23:20:48 by minjungk          #+#    #+#             */
-/*   Updated: 2023/01/24 23:26:23 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/02/03 03:26:51 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define MINISHELL_H
 # include <stdio.h>
 # include <stdlib.h>
+# include <signal.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+# include "libft.h"
 
 #endif
