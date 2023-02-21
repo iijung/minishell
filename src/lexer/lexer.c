6cdec4081ebb773efb9d0365b5bea62506e036ae
Jaemin Jeong <jaemjeon@c6r10s3.42seoul.kr>
2023-02-22 02:00:13 +0900

    remake new lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,4 +1,5 @@
 #include "lexer.h"
+#include "libft.h"
 
 int	is_ifs(char c)
 {
@@ -11,23 +12,15 @@ static void	skip_ifs(const char **string)
 		(*string)++;
 }
 
-int	read_string_norm(const char **p_cursor)
-{
-	while (!(**p_cursor == '"' || **p_cursor == '\'' || **p_cursor == ')' || \
-		**p_cursor == '(' || **p_cursor == '|' || **p_cursor == '\0' || \
-		ft_strncmp(*p_cursor, "&&", 2) == 0 || is_ifs(**p_cursor)))
-	{
-		(*p_cursor)++;
-	}
-	if (**p_cursor == '"')
-		return (E_READ_STRING_IN_DQUOTE);
-	else if (**p_cursor == '\'')
-		return (E_READ_STRING_IN_SQUOTE);
-	else
-	{
-		return (E_READ_STRING_ACC);
-	}
-}
+// int	read_string_norm(const char **p_cursor)
+// {
+// 	while (!(**p_cursor == '"' || **p_cursor == '\'' || **p_cursor == ')' || \
+// 		**p_cursor == '(' || **p_cursor == '|' || **p_cursor == '\0' || \
+// 		ft_strncmp(*p_cursor, "&&", 2) == 0 || is_ifs(**p_cursor)))
+// 	{
+// 		(*p_cursor)++;
+// 	}
+// }
 
 int	read_string_in_quote(const char **p_cursor, char quote)
 {
@@ -63,49 +56,58 @@ int	set_read_string_state(char c)
 }
 
 void	lex_add_token(t_lex_token **lst_lex_token, const char *s_str, int len, \
-	int read_state)
+	int token_type)
 {
 	t_lex_token	*new_token;
 
 	new_token = ft_calloc(1, sizeof(t_lex_token));
 	if (new_token == NULL)
 		exit(errno);
-	if (read_state == E_READ_STRING_ACC)
+	if (token_type == E_STRING)
 	{
 		new_token->type = E_STRING;
 		new_token->string = ft_substr(s_str, 0, len);
 		if (new_token->string == NULL)
 			exit(errno);
-		ft_lstadd_back((t_list **)lst_lex_token, (t_list *)new_token);
 	}
 	else
-	{
-		new_token->type = E_ERROR;
-		ft_lstadd_front((t_list **)lst_lex_token, (t_list *)new_token);
-	}
+		new_token->type = E_IFS;
+	ft_lstadd_back((t_list **)lst_lex_token, (t_list *)new_token);
 }
 
 void	read_string(const char **cursor, t_lex_token **lst_lex_token)
 {
 	const char			*s_str = *cursor;
-	int					read_state;
+	int					token_type;
 
-	read_state = set_read_string_state(**cursor);
-	while (read_state != E_READ_STRING_ACC && read_state != E_READ_STRING_ERROR)
+	if (is_ifs(**cursor))
 	{
-		if (read_state == E_READ_STRING_NORM)
-			read_state = read_string_norm(cursor);
-		else if (read_state == E_READ_STRING_IN_DQUOTE)
-			read_state = read_string_in_quote(cursor, '"');
-		else if (read_state == E_READ_STRING_IN_SQUOTE)
-			read_state = read_string_in_quote(cursor, '\'');
-		else
-			break ;
+		token_type = E_IFS;
+		skip_ifs(cursor);
 	}
-	lex_add_token(lst_lex_token, s_str, *cursor - s_str, read_state);
+	else if (**cursor == '"' || **cursor == '\'')
+	{
+		*cursor = ft_strchr(s_str + 1, **cursor);
+		if (*cursor == NULL)
+			token_type = E_ERROR;
+		else
+		{
+			token_type = E_STRING;
+			(*cursor)++;
+		}
+	}
+	else
+	{
+		token_type = E_STRING;
+		while (!(**cursor == '"' || **cursor == '\'' || **cursor == ')' ||
+				 **cursor == '(' || **cursor == '|' || **cursor == '\0' ||
+				 ft_strncmp(*cursor, "&&", 2) == 0 || is_ifs(**cursor)))
+			(*cursor)++;
+	}
+	lex_add_token(lst_lex_token, s_str, *cursor - s_str, token_type);
 }
 
-int	read_operator(t_lex_token **lst_lex_token, int type)
+void	read_operator(const char **input, t_lex_token **lst_lex_token, int type)
 {
 	t_lex_token	*new_token;
 
@@ -114,34 +116,51 @@ int	read_operator(t_lex_token **lst_lex_token, int type)
 		exit(errno);
 	new_token->type = type;
 	ft_lstadd_back((t_list **)lst_lex_token, (t_list *)new_token);
-	if (type == E_PIPE || type == E_SUB_S || type == E_SUB_E)
-		return (1);
-	else if (type == E_AND || type == E_OR)
-		return (2);
+	if (type == E_AND || type == E_OR || type == E_APPEND || type == E_HEREDOC)
+		(*input) += 2;
 	else
-		return (0);
+		(*input) += 1;
+}
+
+static int	get_next_type(const char *input)
+{
+	if (*input == '|')
+		return (E_PIPE);
+	else if (*input == '(')
+		return (E_SUB_S);
+	else if (*input == ')')
+		return (E_SUB_E);
+	else if (*input == '<')
+		return (E_READ);
+	else if (*input == '>')
+		return (E_WRITE);
+	else if (ft_strncmp(input, "<<", 2) == 0)
+		return (E_HEREDOC);
+	else if (ft_strncmp(input, ">>", 2) == 0)
+		return (E_APPEND);
+	else if (ft_strncmp(input, "||", 2) == 0)
+		return (E_OR);
+	else if (ft_strncmp(input, "&&", 2) == 0)
+		return (E_AND);
+	else if (is_ifs(*input))
+		return (E_IFS);
+	else
+		return (E_STRING);
 }
 
 t_lex_token	*lexer(const char *input)
 {
-	t_lex_token	*lst_lex_token;
+	t_parse	info_parse;
+	int		next_token_type;
 
-	lst_lex_token = NULL;
+	ft_memset(&info_parse, 0, sizeof(t_parse));
 	while (input && *input)
 	{
-		skip_ifs(&input);
-		if (ft_strncmp(input, "||", 2) == 0)
-			input += read_operator(&lst_lex_token, E_OR);
-		else if (ft_strncmp(input, "&&", 2) == 0)
-			input += read_operator(&lst_lex_token, E_AND);
-		else if (*input == '|')
-			input += read_operator(&lst_lex_token, E_PIPE);
-		else if (*input == ')')
-			input += read_operator(&lst_lex_token, E_SUB_E);
-		else if (*input == '(')
-			input += read_operator(&lst_lex_token, E_SUB_S);
+		next_token_type = get_next_type(input);
+		if (next_token_type == E_STRING || next_token_type == E_IFS)
+			read_string(&input, &(info_parse.lst_token));
 		else
-			read_string(&input, &lst_lex_token);
+			read_operator(&input, &(info_parse.lst_token), next_token_type);
 	}
-	return (lst_lex_token);
+	return (info_parse.lst_token);
 }
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -10,7 +10,13 @@ enum
 	E_OR = 1 << 5,
 	E_SUB_S = 1 << 6,
 	E_SUB_E = 1 << 7,
-	E_ERROR = 1 << 8
+	E_IFS = 1 << 8, // 256
+	E_READ = 1 << 9,
+	E_HEREDOC = 1 << 10,
+	E_WRITE = 1 << 11,
+	E_APPEND = 1 << 12,
+	E_REDIRECT = E_READ | E_HEREDOC | E_WRITE | E_APPEND,
+	E_ERROR = 1 << 13
 };
 
 enum
@@ -28,5 +34,26 @@ typedef struct s_lex_token
 	struct s_lex_token	*next;
 	unsigned int		type;
 }	t_lex_token;
+
+typedef struct s_parse_tree
+{
+	unsigned int		type;
+	struct s_parse_tree	*parent;
+	struct s_parse_tree	*child[3];
+}	t_parse_tree;
+
+typedef struct s_parse
+{
+	t_lex_token		*lst_token;
+	t_parse_tree	*tree_token;
+}	t_parse;
+
+typedef struct s_terminal_node
+{
+	char			*string;
+	unsigned int	type;
+	t_parse			*parent;
+}	t_terminal_node;
+
 
 t_lex_token	*lexer(const char *input);
