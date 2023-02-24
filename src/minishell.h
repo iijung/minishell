cf837c611fc27f419bd7cbc6bfb319b8f0e96940
iijung <987kmj4@naver.com>
2023-02-24 17:37:13 +0900

    feat: Add environment hash table

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 23:20:48 by minjungk          #+#    #+#             */
-/*   Updated: 2023/02/06 02:08:43 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/02/24 16:31:33 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include "libft.h"
+
+# define HASH_MAX	10
+
+struct s_minishell
+{
+	int		exit_status;
+	t_list	*environ[HASH_MAX];
+};
 
 #endif
