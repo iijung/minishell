f592d57415311d2ee79674b3c10a84fa7962c298
iijung <987kmj4@naver.com>
2023-04-12 00:49:26 +0900

    fix: if call exit, occur segfault

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 04:37:34 by minjungk          #+#    #+#             */
-/*   Updated: 2023/02/09 08:00:34 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/04/12 00:48:53 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 
 int	builtin_exit(const char **argv)
 {
-	const int	exit_status = ft_atoi(argv[1]);
+	int	exit_status;
 
+	exit_status = EXIT_SUCCESS;
+	if (argv[1])
+		exit_status = ft_atoi(argv[1]);
 	ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (argv[1] && argv[2])
 	{
