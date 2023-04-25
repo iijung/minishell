fb75b7adb369fdcd14304b93df2c93eea693cc7b
iijung <987kmj4@naver.com>
2023-04-25 15:57:10 +0900

    refactor: cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,60 +6,36 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 23:08:02 by minjungk          #+#    #+#             */
-/*   Updated: 2023/04/24 17:41:07 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/04/25 16:00:37 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include <dirent.h>
 
-static int	_can_change_path(char *cdpath, char *path)
-{
-	size_t			len;
-	DIR				*dir;
-	struct dirent	*dirent;
-	char			dirname[PATH_MAX];
-
-	dir = opendir(cdpath);
-	len = ft_strlcpy(dirname, path, sizeof(dirname));
-	if (len > 0 && dirname[len - 1] == '/')
-		dirname[len - 1] = '\0';
-	while (dir)
-	{
-		dirent = readdir(dir);
-		if (dirent == NULL)
-			break ;
-		if (dirent->d_type == DT_DIR
-			&& ft_strlen(dirent->d_name) == ft_strlen(dirname)
-			&& ft_strncmp(dirent->d_name, dirname, ft_strlen(dirname)) == 0)
-		{
-			closedir(dir);
-			return (1);
-		}
-	}
-	closedir(dir);
-	return (0);
-}
-
-static void	_set_with_cdpath(char *currpath, t_env **table, char *path)
+static void	_set_with_prefix(char *currpath, char *prefix, char *path)
 {
 	int			i;
+	DIR			*dir;
 	char		**sp;
-	char *const	cdpath = env_get_val(table, "CDPATH");
+	char		dirname[PATH_MAX];
 
-	ft_memset(currpath, 0, PATH_MAX);
-	if (cdpath == NULL)
-		sp = ft_split(".", ':');
+	if (prefix == NULL)
+		sp = ft_split("./", ':');
 	else
-		sp = ft_split(cdpath, ':');
+		sp = ft_split(prefix, ':');
 	i = 0;
 	while (sp && sp[i])
 	{
-		if (currpath[0] == '\0' && _can_change_path(sp[i], path))
+		if (currpath[0] == '\0')
 		{
-			ft_strlcpy(currpath, sp[i], PATH_MAX);
-			ft_strlcat(currpath, "/", PATH_MAX);
-			ft_strlcat(currpath, path, PATH_MAX);
+			ft_strlcpy(dirname, sp[i], PATH_MAX);
+			ft_strlcat(dirname, "/", PATH_MAX);
+			ft_strlcat(dirname, path, PATH_MAX);
+			dir = opendir(dirname);
+			if (dir)
+				ft_strlcpy(currpath, dirname, PATH_MAX);
+			closedir(dir);
 		}
 		free(sp[i++]);
 	}
@@ -75,13 +51,14 @@ static int	_cd(t_env **table, char *path)
 		path = env_get_val(table, "HOME");
 	if (path == NULL || *path == '\0')
 		return (EXIT_FAILURE);
+	getcwd(oldpath, PATH_MAX);
+	ft_memset(currpath, 0, PATH_MAX);
 	if (*path == '/'
 		|| !ft_strncmp(path, ".", 2) || !ft_strncmp(path, "./", 2)
 		|| !ft_strncmp(path, "..", 3) || !ft_strncmp(path, "../", 3))
 		ft_strlcpy(currpath, path, PATH_MAX);
 	else
-		_set_with_cdpath(currpath, table, path);
-	getcwd(oldpath, PATH_MAX);
+		_set_with_prefix(currpath, env_get_val(table, "CDPATH"), path);
 	if (chdir(currpath) == -1)
 	{
 		perror("minishell: cd");
