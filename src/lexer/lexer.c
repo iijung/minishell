5e90a062deeb4598a6d2dc81136eb601e9d935a3
Jaemin Jeong <jaemjeon@c1r2s6.42seoul.kr>
2023-05-16 19:53:46 +0900

    feature: dollar + question => type : env, data : ?
    $?? -> string: $ + string: ? + string: ?
    
    $?? -> env: ? + string: ?

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 22:28:04 by minjungk          #+#    #+#             */
-/*   Updated: 2023/05/15 17:59:18 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:51:35 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,20 +87,19 @@ static enum e_lexeme	get_lexeme(char *data)
 
 static char	*case_of_environment(
 t_lex_lst **lst,
-char **curr,
-int *in_dquote_flag)
+char **curr)
 {
 	char *const			base = *curr;
 	const enum e_lexeme	type = get_lexeme(base);
 
-	if (curr[1] == '?')
+	if ((*curr)[1] == '?')
 		return (add_token(lst, type, 2, base));
-	if (ft_isalnum(curr[1]) == 0 && curr[1] != '_')
+	if (ft_isalnum((*curr)[1]) == 0 && (*curr)[1] != '_')
 		return (add_token(lst, LEXEME_STRING, 1, base));
 	++(*curr);
 	while (curr[0] && (ft_isalnum((*curr)[0]) || (*curr)[0] == '_'))
 		++(*curr);
-	return (add_token(lst, type, curr - base - 1, base + 1));
+	return (add_token(lst, type, *curr - base - 1, base + 1));
 }
 
 static char	*lex_token(t_lex_lst **lst, char *curr, int *in_dquote_flag)
@@ -109,7 +108,7 @@ static char	*lex_token(t_lex_lst **lst, char *curr, int *in_dquote_flag)
 	const enum e_lexeme	type = get_lexeme(base);
 
 	if (type == LEXEME_ENVIRONMENT)
-		return (case_of_environment(lst, &curr, in_dquote_flag));
+		return (case_of_environment(lst, &curr));
 	else if (type == LEXEME_QUOTE && *in_dquote_flag == 0)
 	{
 		curr = ft_strchr(base + 1, g_lexeme[type].data[0]);
