27b6f6b2208a7d484aa8a1036c7155546d36e988
Jaemin Jeong <jaemjeon@c1r2s6.42seoul.kr>
2023-05-17 14:11:59 +0900

    feat: add numeric argument error in exit
    ```
    minishell$ exit a
    exit
    $ echo $?
    0
    ```
    
    ```
    minishell$ exit a
    minishell: exit: a: numeric argument required
    minishell$ echo $?
    1
    ```

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 04:37:34 by minjungk          #+#    #+#             */
-/*   Updated: 2023/05/17 11:33:23 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/05/17 14:04:45 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,22 @@
 
 int	builtin_exit(t_env **table, int argc, char **argv)
 {
+	int	i;
 	int	exit_status;
 
 	if (table == NULL || argc < 0 || argv == NULL)
 		return (EXIT_FAILURE);
+	i = 0;
+	while (argv && argv[1] && argv[1][i])
+	{
+		if (ft_isdigit(argv[1][i++]) == 0)
+		{
+			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+			ft_putstr_fd(argv[1], STDERR_FILENO);
+			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+			return (EXIT_FAILURE);
+		}
+	}
 	exit_status = EXIT_SUCCESS;
 	if (argv && argv[1])
 		exit_status = ft_atoi(argv[1]);
