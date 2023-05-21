e7d4d1cc8688aeade91cc42afc2198316f611fb6
Jaemin Jeong <jaemjeon@c1r2s6.42seoul.kr>
2023-05-21 16:05:02 +0900

    fix

--- src/parse/syntax.c
+++ src/parse/syntax.c
@@ -6,7 +6,7 @@
 /*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:51:29 by jaemjeon          #+#    #+#             */
-/*   Updated: 2023/05/21 15:57:43 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2023/05/21 16:04:10 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ int	is_syntax_error(t_parse *root)
 	t_lex_lst	*lex_lst;
 	t_s_lex		*lex_data;
 
-	if (root->is_subshell && root->node)
-		return (1);
+	if (root->is_subshell)
+	{
+		if (root->node)
+			return (1);
+		return (0);
+	}
 	else
 	{
 		if (root->right && is_syntax_error(root->right))
