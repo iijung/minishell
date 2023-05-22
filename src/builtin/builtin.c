ed240778e93c5d0978b69eb93318b9e4c4016007
Jaemin Jeong <jaemjeon@c1r2s6.42seoul.kr>
2023-05-22 10:54:35 +0900

    fix: edit no_builtin command logic
    1. check path_cmd or general_cmd
    
    (path_cmd)
    
    2-1 : check file is there
    
    2-2(found file) : execve!
    
    2-3(file not found || execve fail) : print error && exit correct exit_status
    
    (general_cmd)
    
    2-1 : loop path and check file is there.
    
    2-2(find file) : execve! if execve failed exit 0
    
    2-3-1(found file) : try execve, if failed exit 255
    
    2-3-2(file not found) : print error && exit 126 || exit 127

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 22:32:56 by minjungk          #+#    #+#             */
-/*   Updated: 2023/05/22 00:07:31 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/05/22 10:54:15 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+#include "sys/stat.h"
+#include <errno.h>
 
 extern int	builtin_pwd(void);
 extern int	builtin_cd(t_env **table, int argc, char **argv);
@@ -20,32 +22,56 @@ extern int	builtin_export(t_env **table, int argc, char **argv);
 extern int	builtin_unset(t_env **table, int argc, char **argv);
 extern int	builtin_env(t_env **table, int argc, char **argv);
 
+static char	*strjoin3(char *s1, char *s2, char *s3)
+{
+	char	*rtn;
+	char	*tmp;
+
+	rtn = ft_strjoin(s1, s2);
+	tmp = rtn;
+	rtn = ft_strjoin(tmp, s3);
+	free(tmp);
+	return (rtn);
+}
+
+static void	search_path(t_env **table, char **argv, char **envp)
+{
+	int			i;
+	char		**sp;
+	char		*execve_path;
+	struct stat	st;
+
+	sp = ft_split(env_get_val(table, "PATH"), ':');
+	i = 0;
+	while (sp && sp[i])
+	{
+		execve_path = strjoin3(sp[i], "/", argv[0]);
+		if (stat(execve_path, &st) == 0)
+			exit(execve(execve_path, argv, envp));
+		free(sp[i++]);
+	}
+}
+
 static int	no_builtin(t_env **table, char **argv)
 {
-	int		i;
-	pid_t	pid;
-	char	**sp;
-	char	**envp;
-	char	command[PATH_MAX];
+	pid_t		pid;
+	char		**envp;
+	struct stat	st;
 
 	pid = fork();
 	if (pid)
 		return (waitpid_ignore_signal(pid));
 	envp = env_get_arr(table);
-	execve(argv[0], argv, envp);
-	i = 0;
-	sp = ft_split(env_get_val(table, "PATH"), ':');
-	while (sp && sp[i])
+	if (ft_strchr(argv[0], '/') != NULL)
 	{
-		ft_strlcpy(command, sp[i], PATH_MAX);
-		ft_strlcat(command, "/", PATH_MAX);
-		ft_strlcat(command, argv[0], PATH_MAX);
-		execve(command, argv, envp);
-		free(sp[i++]);
+		if (stat(argv[0], &st) == 0)
+			execve(argv[0], argv, envp);
 	}
-	free(sp);
-	ft_putstr_fd(argv[0], STDERR_FILENO);
-	ft_putstr_fd(": command not found\n", STDERR_FILENO);
+	else
+		search_path(table, argv, envp);
+	perror(argv[0]);
+	if (errno == EISDIR || errno == EACCES)
+		exit(126);
 	exit(127);
 }
 
